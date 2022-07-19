1f7249c6a9adea645369cc526768867e421ceb0b
闲心 <11329420+xianxincoder@user.noreply.gitee.com>
2022-07-19 11:45:47 +0000

    feat: 群聊，群友发送相同信息机器人也发送相同信息

--- apps/chat.js
+++ apps/chat.js
@@ -1,7 +1,8 @@
 import { segment } from "oicq";
 import plugin from "../../../lib/plugins/plugin.js";
 
-let tempMsgList = []; // [{qq: xxx, message: xxx, type: xxx}]
+// 存放接收到的两条信息
+let tempMsg = {};
 
 export class chat extends plugin {
   constructor() {
@@ -9,7 +10,7 @@ export class chat extends plugin {
       name: "聊天",
       dsc: "群里好友发的信息，相关处理",
       event: "message.group",
-      priority: 800,
+      priority: 999,
       rule: [
         {
           reg: "^.*$",
@@ -24,23 +25,56 @@ export class chat extends plugin {
    * 如果不同群友发送信息相同两次 那么小机器人也发送一次相同的信息
    */
   async repeat() {
+    // 如果不是群聊那么直接停止
     if (!this.e.isGroup) return;
 
-    console.log(this.e);
     const info = this.e;
 
-    tempMsgList.push({ qq: info.user_id });
+    // 如果消息过于复杂那么直接停止
+    if (info.message.length > 1) return;
 
-    /** 冷却cd 30s */
-    let cd = 30;
+    const message = info.message[0];
 
-    /** cd */
-    let key = `Yz:repeat:${this.e.group_id}`;
-    if (await redis.get(key)) return;
-    redis.set(key, "1", { EX: cd });
+    let tempMsgList = tempMsg[info.group_id] || [];
 
-    return;
+    if (message.type === "text" || message.type === "face") {
+      tempMsgList.push({
+        qq: info.user_id,
+        message: message.text,
+        type: message.type,
+      });
+    } else if (message.type === "image") {
+      tempMsgList.push({
+        qq: info.user_id,
+        message: message.file,
+        type: message.type,
+      });
+    } else {
+      return;
+    }
 
-    this.reply("开发中");
+    tempMsg[info.group_id] = tempMsgList;
+
+    if (tempMsgList.length < 3) return;
+
+    if (tempMsgList.length > 3) {
+      tempMsgList = tempMsgList.slice(tempMsgList.length - 3);
+      tempMsg[info.group_id] = tempMsgList;
+    }
+
+    const messageSet = new Set(tempMsgList.map((item) => item.message));
+
+    if (messageSet.size < 3) {
+      /** 冷却cd 300s */
+      let cd = 300;
+
+      /** cd */
+      let key = `Yz:repeat:${this.e.group_id}`;
+      if (await redis.get(key)) return;
+      redis.set(key, "1", { EX: cd });
+      tempMsg[info.group_id] = [];
+
+      this.reply(info.message);
+    }
   }
 }
