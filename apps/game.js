5be281a74b0cc6e5fe1795bf63339275345105cd
闲心 <11329420+xianxincoder@user.noreply.gitee.com>
2022-07-30 14:50:14 +0000

    feat: 添加群战的初步框架

--- apps/game.js
+++ apps/game.js
@@ -1,5 +1,6 @@
 import plugin from "../../../lib/plugins/plugin.js";
 import fs from "node:fs";
+import { segment } from "oicq";
 
 let pkArr = {};
 
@@ -8,76 +9,24 @@ let pkArr = {};
  * {1484288448: {exp: 0, nick: ''}}
  */
 
-/**
- * @数据
- * {
-  post_type: 'message',
-  message_id: 'GXLow1h4ccAAADymf/yg82Lk/wwB',
-  user_id: 1484288448,
-  time: 1659174668,
-  seq: 15526,
-  rand: 2147262707,
-  font: 'Helvetica',
-  message: [
-    { type: 'text', text: '#pk' },
-    { type: 'at', qq: 2243784785, text: '@派蒙' },
-    { type: 'text', text: '' }
-  ],
-  raw_message: '#pk@派蒙 ',
-  message_type: 'group',
-  sender: {
-    user_id: 1484288448,
-    nickname: '闲心',
-    card: '闲心',
-    sex: 'unknown',
-    age: 0,
-    area: '',
-    level: 1,
-    role: 'owner',
-    title: ''
-  },
-  group_id: 426961091,
-  group_name: '原神丘丘群',
-  block: false,
-  sub_type: 'normal',
-  anonymous: null,
-  atme: true,
-  atall: false,
-  group: Group {},
-  member: Member {},
-  reply: [AsyncFunction (anonymous)],
-  recall: [Function (anonymous)],
-  self_id: 2243784785,
-  msg: '#pk',
-  atBot: true,
-  logText: '[原神丘丘群(闲心)]',
-  isGroup: true,
-  isMaster: true,
-  replyNew: [Function (anonymous)],
-  logFnc: '[pk游戏][pk]',
-  checkAuth: [AsyncFunction (anonymous)],
-  getMysApi: [AsyncFunction (anonymous)]
-}
- */
-
 export class game extends plugin {
   constructor(e) {
     super({
-      name: "pk游戏",
-      dsc: "pk小游戏",
+      name: "群战小游戏",
+      dsc: "群战小游戏",
       event: "message.group",
       priority: 500,
       rule: [
         {
-          reg: "^#我的pk$",
+          reg: "^#(我的|加入)?群战(信息)?$",
           fnc: "mypk",
         },
         {
-          reg: "^#pk榜$",
+          reg: "^#战榜$",
           fnc: "rank",
         },
         {
-          reg: "^#pk$",
+          reg: "^(#)?战$",
           fnc: "pk",
         },
       ],
@@ -99,50 +48,85 @@ export class game extends plugin {
 
   async mypk() {
     await this.getGroupId();
-    if (!this.group_id) return false;
+    if (!this.group_id) return;
+
+    this.initPkArr();
 
     if (!pkArr[this.group_id]) {
       this.register();
     }
 
-    let pkInfo = pkArr[this.group_id].get(this.e.user_id);
+    let pkInfo = pkArr[this.group_id].get(String(this.e.user_id));
 
     if (!pkInfo) {
       this.register();
-      pkInfo = { nick: this.e.sender.card, exp: 0 };
+      pkInfo = { nick: this.e.sender.card || this.e.user_id, exp: 100 };
     }
 
-    this.reply(`昵称：${pkInfo.nick}\n等级：${pkInfo.exp}`);
+    this.reply(`昵称：${pkInfo.nick}\n战力：${pkInfo.exp}`);
   }
 
   async pk() {
     await this.getGroupId();
-    if (!this.group_id) return false;
+    if (!this.group_id) return;
 
     this.initPkArr();
 
-    console.log(this.e);
-
     if (!pkArr[this.group_id]) {
-      this.reply("首次战斗，请先使用#我的pk注册pk信息");
-      return false;
+      this.reply("首次战斗，请先使用#加入群战注册群战信息");
+      return;
     }
 
-    let pkInfo = pkArr[this.group_id].get(this.e.user_id);
+    let selfInfo = pkArr[this.group_id].get(String(this.e.user_id));
 
-    if (!pkInfo) {
-      this.reply("首次战斗，请先使用#我的pk注册pk信息");
-      return false;
+    if (!selfInfo) {
+      this.reply("首次战斗，请先使用#加入群战注册群战信息");
+      return;
     }
 
-    console.log(this.e);
+    const enemy = this.getEnemy();
 
-    await this.reply("123");
+    if (!enemy) {
+      this.reply("没有对手的战斗，如何战斗。请发送#战@某位群友");
+      return;
+    }
+
+    let enemyInfo = pkArr[this.group_id].get(String(enemy));
+
+    if (!enemyInfo) {
+      this.reply("对手未注册群战信息，请先让对手使用#加入群战注册群战信息");
+      return;
+    }
+
+    const retData = this.pkHandle(
+      { ...selfInfo, user_id: this.e.user_id },
+      { ...enemyInfo, user_id: enemy }
+    );
+
+    const { winner, loser } = retData;
+
+    const message = [
+      segment.at(this.e.user_id, this.e.sender.card || this.e.user_id),
+    ];
+
+    if ((this.e.sender.card || this.e.user_id) === winner.nick) {
+      message.push(" 完胜");
+      message.push(
+        `\n战胜了对手，并获得战力${winner.tempExp}点，当前战力为${winner.exp}`
+      );
+    } else {
+      message.push(" 惜败");
+      message.push(
+        `\n惜败，并损失战力${loser.tempExp}点，当前战力为${loser.exp}`
+      );
+    }
+
+    await this.reply(message);
   }
 
   async rank() {
     await this.getGroupId();
-    if (!this.group_id) return false;
+    if (!this.group_id) return;
   }
 
   async register() {
@@ -150,12 +134,105 @@ export class game extends plugin {
 
     if (!pkArr[this.group_id]) pkArr[this.group_id] = new Map();
 
-    pkArr[this.group_id].set(this.e.user_id, {
-      nick: this.e.sender.card,
-      exp: 0,
+    pkArr[this.group_id].set(String(this.e.user_id), {
+      nick: this.e.sender.card || this.e.user_id,
+      exp: 100,
     });
 
     this.saveJson();
+  }
+
+  /** pk处理 */
+  pkHandle(self, enemy) {
+    if (!pkArr[this.group_id]) pkArr[this.group_id] = new Map();
+
+    const { exp: selfExp } = self;
+
+    const { exp: enemyExp } = enemy;
+
+    let winner = null;
+    let loser = null;
+    let loserReduce = 0;
+
+    const totalExp = selfExp + enemyExp;
+
+    let probability = Number(selfExp / totalExp).toFixed(2);
+
+    let randomNum = Math.random().toFixed(2);
+
+    let tempSelfExp = selfExp;
+    let tempEnemyExp = enemyExp;
+
+    if (randomNum > probability) {
+      // 输了
+      // 扣减的经验值为对手的1/10经验
+      let reduce = Math.round(selfExp * 0.1);
+
+      if (reduce > enemyExp) {
+        reduce = enemyExp;
+      }
+
+      tempSelfExp = selfExp - reduce;
+      tempEnemyExp = enemyExp + reduce;
+      loserReduce = reduce;
+
+      if (tempSelfExp < 10) {
+        loserReduce = tempSelfExp - 10;
+        tempSelfExp = 10;
+      }
+
+      winner = { ...enemy, exp: tempEnemyExp, tempExp: reduce };
+      loser = { ...self, exp: tempSelfExp, tempExp: loserReduce };
+    } else {
+      // 赢了
+      // 增加的经验值为对手的1/10经验
+      let add = Math.round(enemyExp * 0.1);
+
+      // 如果增加的经验值比自己全部的经验都多，那么自己的经验最多翻倍
+      if (add > selfExp) {
+        add = selfExp;
+      }
+
+      tempSelfExp = selfExp + add;
+
+      tempEnemyExp = enemyExp - add;
+
+      loserReduce = add;
+      if (tempEnemyExp < 10) {
+        tempEnemyExp = 10;
+        loserReduce = tempEnemyExp - 10;
+      }
+
+      winner = { ...self, exp: tempSelfExp, tempExp: add };
+      loser = { ...enemy, exp: tempEnemyExp, tempExp: loserReduce };
+    }
+
+    pkArr[this.group_id].set(String(self.user_id), {
+      nick: self.nick,
+      exp: tempSelfExp,
+    });
+
+    pkArr[this.group_id].set(String(enemy.user_id), {
+      nick: enemy.nick,
+      exp: tempEnemyExp,
+    });
+
+    this.saveJson();
+    return { winner, loser };
+  }
+
+  getEnemy() {
+    /** 添加内容 */
+    let message = this.e.message;
+
+    let enemy = "";
+
+    for (let i in message) {
+      if (message[i].type == "at") {
+        enemy = message[i].qq;
+      }
+    }
+    return enemy;
   }
 
   /** 初始化已添加内容 */
@@ -172,12 +249,12 @@ export class game extends plugin {
     try {
       let pkMapJson = JSON.parse(fs.readFileSync(path, "utf8"));
       for (let key in pkMapJson) {
-        pkArr[this.group_id].set(String(key), text[key]);
+        pkArr[this.group_id].set(String(key), pkMapJson[key]);
       }
     } catch (error) {
       logger.error(`json格式错误：${path}`);
       delete pkArr[this.group_id];
-      return false;
+      return;
     }
   }
 
@@ -196,7 +273,7 @@ export class game extends plugin {
       return this.group_id;
     }
 
-    return false;
+    return;
   }
 
   /** 保存json文件 */
