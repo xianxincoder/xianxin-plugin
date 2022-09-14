c9cb8ad680f84c2423d41db3b2f9386f8ad720db
闲心 <11329420+xianxincoder@user.noreply.gitee.com>
2022-09-14 07:29:34 +0000

    feat: 添加 #色榜 查看积分排名
    Signed-off-by: 闲心 <11329420+xianxincoder@user.noreply.gitee.com>

--- apps/seecolor.js
+++ apps/seecolor.js
@@ -1,5 +1,8 @@
 import plugin from "../../../lib/plugins/plugin.js";
 import { segment } from "oicq";
+import fs from "node:fs";
+import Game from "../model/game.js";
+import lodash from "lodash";
 import Seecolor from "../model/seecolor.js";
 import puppeteer from "../../../lib/puppeteer/puppeteer.js";
 import common from "../../../lib/common/common.js";
@@ -15,6 +18,9 @@ let gameing = {};
 
 // 定时器如果一段时间没有人回应那么清除进行中状态
 let seecolorTimer = {};
+
+// color信息存放
+let colorArr = {};
 
 /**
  * 给我点颜色看看小游戏处理
@@ -35,9 +41,20 @@ export class seecolor extends plugin {
           reg: "^#*块\\s*[0-9]*$",
           fnc: "click",
         },
+        {
+          reg: "^#*色榜$",
+          fnc: "colorRank",
+        },
       ],
     });
+    this.path = "./data/colorJson/";
     this.gameSetData = xxCfg.getConfig("game", "set");
+  }
+
+  async init() {
+    if (!fs.existsSync(this.path)) {
+      fs.mkdirSync(this.path);
+    }
   }
 
   /** 群号key */
@@ -99,7 +116,15 @@ export class seecolor extends plugin {
           return b.score - a.score;
         });
 
-        const scoreMessage = ["本局结束，得分如下"];
+        this.sortHandle();
+
+        const scoreMessage = [
+          `很遗憾${
+            this.gameSetData.seecolorOverTime || 45
+          }s内没有人答对，本局结束。\n本局答案为：块${
+            seecolorState[this.group_id].randomNum + 1
+          }，得分排名如下`,
+        ];
 
         arr.map((item) => {
           scoreMessage.push(`${item.userName} ${item.score}分`);
@@ -152,7 +177,7 @@ export class seecolor extends plugin {
 
     if (scoreData[this.group_id].has(this.e.user_id)) {
       scoreData[this.group_id].set(this.e.user_id, {
-        user_id: this.e.user_id,
+        user_id: String(this.e.user_id),
         score:
           scoreData[this.group_id].get(this.e.user_id).score +
           seecolorState[this.group_id].current *
@@ -161,7 +186,7 @@ export class seecolor extends plugin {
       });
     } else {
       scoreData[this.group_id].set(this.e.user_id, {
-        user_id: this.e.user_id,
+        user_id: String(this.e.user_id),
         score:
           seecolorState[this.group_id].current *
           seecolorState[this.group_id].current,
@@ -180,6 +205,8 @@ export class seecolor extends plugin {
         arr = arr.sort((a, b) => {
           return b.score - a.score;
         });
+
+        this.sortHandle();
 
         const scoreMessage = [
           `很遗憾${
@@ -233,6 +260,104 @@ export class seecolor extends plugin {
     await common.sleep(600);
 
     this.e.reply(img);
+  }
+
+  async colorRank() {
+    await this.getGroupId();
+    if (!this.group_id) return;
+
+    const players = this.getPlayers();
+
+    if (!players || !players.length) {
+      this.reply(`未找到玩家数据`);
+      return;
+    }
+
+    const sortScorePlayers = players.sort(function (a, b) {
+      return b.score - a.score;
+    });
+
+    const data = await new Game(this.e).getRankData(
+      sortScorePlayers.slice(0, this.gameSetData.limitTop || 20)
+    );
+
+    let img = await puppeteer.screenshot("rank", {
+      ...data,
+      type: "color",
+      limitTop: this.gameSetData.limitTop || 20,
+    });
+    this.e.reply(img);
+  }
+
+  sortHandle() {
+    const currentPlayers = [];
+    for (let [key, value] of scoreData[this.group_id]) {
+      currentPlayers.push(value);
+    }
+
+    const players = this.getPlayers() || [];
+
+    const allPlayers = [...players, ...currentPlayers];
+
+    let sortScorePlayers = allPlayers.sort(function (a, b) {
+      return b.score - a.score;
+    });
+
+    sortScorePlayers = lodash.uniqBy(sortScorePlayers, "user_id");
+
+    sortScorePlayers.map((item) => {
+      colorArr[this.group_id].set(String(item.user_id), item);
+    });
+
+    this.saveJson();
+  }
+
+  getPlayers() {
+    this.initColorArr();
+
+    if (!colorArr[this.group_id]) colorArr[this.group_id] = new Map();
+
+    let playerArr = [];
+
+    for (let [k, v] of colorArr[this.group_id]) {
+      playerArr.push({ ...v, user_id: k });
+    }
+    return playerArr;
+  }
+
+  /** 初始化群战信息 */
+  initColorArr() {
+    colorArr[this.group_id] = new Map();
+
+    let path = `${this.path}${this.group_id}.json`;
+    if (!fs.existsSync(path)) {
+      return;
+    }
+
+    try {
+      let colorMapJson = JSON.parse(fs.readFileSync(path, "utf8"));
+      for (let key in colorMapJson) {
+        colorArr[this.group_id].set(String(key), colorMapJson[key]);
+      }
+      return;
+    } catch (error) {
+      logger.error(`json格式错误：${path}`);
+      delete colorArr[this.group_id];
+      return;
+    }
+  }
+
+  /** 保存json文件 */
+  saveJson() {
+    let obj = {};
+    for (let [k, v] of colorArr[this.group_id]) {
+      obj[k] = v;
+    }
+
+    fs.writeFileSync(
+      `${this.path}${this.group_id}.json`,
+      JSON.stringify(obj, "", "\t")
+    );
   }
 
   /**
