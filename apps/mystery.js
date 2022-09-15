b6bdc220e8f2d4a84a7ddc735483aa2cacdeecf7
闲心 <11329420+xianxincoder@user.noreply.gitee.com>
2022-09-15 06:48:27 +0000

    feat: 神秘代码支持读取本地文件源并单独放到一个模块中，方便禁用
    Signed-off-by: 闲心 <11329420+xianxincoder@user.noreply.gitee.com>

---
+++ apps/mystery.js
@@ -0,0 +1,290 @@
+import plugin from "../../../lib/plugins/plugin.js";
+import fetch from "node-fetch";
+import { segment } from "oicq";
+import lodash from "lodash";
+import common from "../../../lib/common/common.js";
+import xxCfg from "../model/xxCfg.js";
+import fs from "node:fs";
+
+//项目路径
+const _path = process.cwd();
+
+/**
+ * 初始化工具设置文件
+ */
+let toolsSetFile = "./plugins/xianxin-plugin/config/tools.set.yaml";
+if (!fs.existsSync(toolsSetFile)) {
+  fs.copyFileSync(
+    "./plugins/xianxin-plugin/defSet/tools/set.yaml",
+    toolsSetFile
+  );
+}
+
+let fileArr = new Set();
+
+let urlTypeCache = {};
+
+export class mystery extends plugin {
+  constructor() {
+    super({
+      name: "神秘指令",
+      dsc: "处理神秘指令代码",
+      event: "message",
+      priority: 5000,
+      rule: [
+        {
+          reg: "^#*(woc|卧槽)$",
+          fnc: "woc",
+          permission: "master",
+        },
+      ],
+    });
+
+    /** 读取工具相关设置数据 */
+    this.toolsSetData = xxCfg.getConfig("tools", "set");
+
+    this.rule[0].permission = this.toolsSetData.permission;
+    this.rule[0].reg = `^#*(${this.toolsSetData.keywords.join("|")})$`;
+
+    if (this.toolsSetData.wocUrl.indexOf("http") == -1) {
+      this.resourcesPath = `${_path}${this.toolsSetData.wocUrl}`;
+    }
+  }
+
+  async init() {
+    /** 读取工具相关设置数据 */
+    this.toolsSetData = xxCfg.getConfig("tools", "set");
+    if (this.toolsSetData.wocUrl.indexOf("http") == -1) {
+      this.resourcesPath = `${_path}${this.toolsSetData.wocUrl}`;
+      this.readdirectory(this.resourcesPath, "img");
+      this.watchFile(this.resourcesPath, "img");
+    }
+  }
+
+  async woc() {
+    const isPrivate = this.e.isPrivate;
+
+    if (!this.toolsSetData.isPrivate && isPrivate) {
+      return "return";
+    }
+
+    // if (this.toolsSetData.permission == "master" && !this.e.isMaster) {
+    //   return "return";
+    // }
+
+    // if (this.toolsSetData.permission == "admin" && !this.e.member.is_admin) {
+    //   return "return";
+    // }
+    // if (this.toolsSetData.permission == "owner" && !this.e.member.is_owner) {
+    //   return "return";
+    // }
+
+    if (this.toolsSetData.cd != 0) {
+      /** cd */
+      let key = `Yz:woc:${this.e.group_id}`;
+      if (await redis.get(key)) return;
+      redis.set(key, "1", { EX: Number(this.toolsSetData.cd) });
+    }
+
+    this.e.reply("触发探索未知的神秘空间，请稍等...");
+    let images = [];
+    const isDimtown = this.toolsSetData.wocUrl.indexOf("dimtown.com") !== -1;
+
+    if (this.resourcesPath && fileArr.size) {
+      images = lodash.sampleSize(
+        Array.from(fileArr),
+        this.toolsSetData.imageCountLimit || 10
+      );
+    } else if (this.toolsSetData.wocUrl.indexOf("/wp-json") !== -1) {
+      const randomMax = isDimtown ? 400 : 600;
+
+      const randomIndex = Math.floor(Math.random() * randomMax) + 1;
+
+      const page = Math.ceil(randomIndex / 10);
+
+      const fetchData = await fetch(`${this.toolsSetData.wocUrl}${page}`);
+      const resJsonData = await fetchData.json();
+
+      const index = randomIndex % 10;
+
+      if (!resJsonData.length) {
+        this.e.reply("额。没有探索到，换个姿势再来一次吧～");
+        return "return";
+      }
+
+      const content = resJsonData[index].content;
+
+      if (!content || !content.rendered) {
+        this.e.reply("额。没有探索到，换个姿势再来一次吧～");
+        return "return";
+      }
+
+      images = this.getImages(content.rendered);
+    } else {
+      if (urlTypeCache[this.toolsSetData.wocUrl] == "buffer") {
+        const image = await this.getBufferImage(`${this.toolsSetData.wocUrl}`);
+        images = [image];
+      } else {
+        try {
+          const fetchData = await fetch(`${this.toolsSetData.wocUrl}`);
+          const resJsonData = await fetchData.json();
+
+          images = this.getJsonImages(JSON.stringify(resJsonData));
+        } catch (error) {
+          urlTypeCache[this.toolsSetData.wocUrl] = "buffer";
+          const image = await this.getBufferImage(
+            `${this.toolsSetData.wocUrl}`
+          );
+          images = [image];
+        }
+      }
+    }
+
+    if (isDimtown && images.length > 1) {
+      images.pop();
+    }
+
+    const imageCountLimit = this.toolsSetData.imageCountLimit || 10;
+
+    if (images.length > imageCountLimit) {
+      images.length = imageCountLimit;
+    }
+
+    const forwarder =
+      this.toolsSetData.forwarder == "bot"
+        ? { nickname: Bot.nickname, user_id: Bot.uin }
+        : {
+            nickname: this.e.sender.card || this.e.user_id,
+            user_id: this.e.user_id,
+          };
+
+    if (images && images.length) {
+      let msgList = [];
+      for (let imageItem of images) {
+        if (isPrivate) {
+          await this.e.reply(segment.image(imageItem), false, {
+            recallMsg: this.toolsSetData.delMsg,
+          });
+          await common.sleep(600);
+        } else {
+          msgList.push({
+            message: segment.image(imageItem),
+            ...forwarder,
+          });
+        }
+      }
+      if (isPrivate) {
+        return;
+      }
+      const res = await this.e.reply(await Bot.makeForwardMsg(msgList), false, {
+        recallMsg: this.toolsSetData.delMsg,
+      });
+      if (!res) {
+        if (!res) {
+          if (this.e.group.is_admin) {
+            if (
+              Number(Math.random().toFixed(2)) * 100 <
+              this.toolsSetData.mute
+            ) {
+              let duration = Math.floor(Math.random() * 600) + 1;
+              this.e.group.muteMember(this.e.sender.user_id, duration);
+              await this.e.reply(
+                `不用等了，你想要的已经被神秘的力量吞噬了～ 并随手将你禁锢${duration}秒`
+              );
+            } else {
+              this.reply("不用等了，你想要的已经被神秘的力量吞噬了～");
+            }
+          } else {
+            this.reply("不用等了，你想要的已经被神秘的力量吞噬了～");
+          }
+        }
+      }
+    } else {
+      this.reply("额。没有探索到，换个姿势再来一次吧～");
+    }
+  }
+
+  readdirectory(dir, type) {
+    let files = fs.readdirSync(dir, { withFileTypes: true });
+    for (let val of files) {
+      let filepath = dir + `/` + val.name;
+      if (!val.isFile()) {
+        this.readdirectory(filepath, type);
+        continue;
+      }
+      let re;
+
+      if (type == "img") {
+        re = new RegExp(`.(jpg|jpeg|png|gif|bmp)$`, "i");
+      }
+      if (!re.test(val.name)) {
+        continue;
+      }
+      fileArr.add(filepath);
+    }
+  }
+
+  watchFile(dir, type) {
+    let fsTimeout = {};
+    let recursive = false;
+    fs.watch(dir, { recursive: recursive }, async (eventType, filename) => {
+      if (fsTimeout[type]) return;
+
+      let re;
+      if (type == "img") {
+        Bot.logger.mark("更新神秘空间图片");
+        re = new RegExp(`.(jpg|jpeg|png|gif|bmp)$`, "i");
+        fileArr.clear();
+      }
+
+      if (!re.test(filename)) return;
+
+      fsTimeout[type] = true;
+
+      setTimeout(async () => {
+        this.readdirectory(dir, type);
+        fsTimeout[type] = null;
+      }, 5000);
+    });
+  }
+
+  getImages(string) {
+    const imgRex = /<img.*?src="(.*?)"[^>]+>/g;
+    const images = [];
+    let img;
+    while ((img = imgRex.exec(string))) {
+      images.push(img[1]);
+    }
+    return images;
+  }
+
+  getJsonImages(string) {
+    const imgRex = /https?:\/\/.*?\.(jpg|JPG|png|PNG|gif|GIF|jpeg|JPEG)/g;
+    const images = [];
+    let img;
+    while ((img = imgRex.exec(string))) {
+      images.push(img[0]);
+    }
+    return images;
+  }
+
+  // 获取图片
+  async getBufferImage(url) {
+    let response = await fetch(url, {
+      method: "get",
+      responseType: "arraybuffer",
+    });
+
+    const buffer = await response.arrayBuffer();
+
+    return (
+      "base64://" +
+      btoa(
+        new Uint8Array(buffer).reduce(
+          (data, byte) => data + String.fromCharCode(byte),
+          ""
+        )
+      )
+    );
+  }
+}
--- apps/tools.js
+++ apps/tools.js
@@ -1,24 +1,7 @@
 import plugin from "../../../lib/plugins/plugin.js";
 import fetch from "node-fetch";
-import { segment } from "oicq";
 import puppeteer from "../../../lib/puppeteer/puppeteer.js";
-import common from "../../../lib/common/common.js";
-import xxCfg from "../model/xxCfg.js";
 import Tools from "../model/tools.js";
-import fs from "node:fs";
-
-/**
- * 初始化工具设置文件
- */
-let toolsSetFile = "./plugins/xianxin-plugin/config/tools.set.yaml";
-if (!fs.existsSync(toolsSetFile)) {
-  fs.copyFileSync(
-    "./plugins/xianxin-plugin/defSet/tools/set.yaml",
-    toolsSetFile
-  );
-}
-
-let urlTypeCache = {};
 
 export class tools extends plugin {
   constructor() {
@@ -33,11 +16,6 @@ export class tools extends plugin {
           fnc: "thumbsUpMe",
         },
         {
-          reg: "^#*(woc|卧槽)$",
-          fnc: "woc",
-          permission: "master",
-        },
-        {
           reg: "^#*(闲心)?发电榜$",
           fnc: "fdrank",
         },
@@ -47,154 +25,11 @@ export class tools extends plugin {
         },
       ],
     });
-
-    /** 读取工具相关设置数据 */
-    this.toolsSetData = xxCfg.getConfig("tools", "set");
-
-    this.rule[1].permission = this.toolsSetData.permission;
-    this.rule[1].reg = `^#*(${this.toolsSetData.keywords.join("|")})$`;
   }
 
   async thumbsUpMe() {
     Bot.pickFriend(this.e.user_id).thumbUp(10);
     this.reply("已给你点赞");
-  }
-
-  async woc() {
-    const isPrivate = this.e.isPrivate;
-
-    if (!this.toolsSetData.isPrivate && isPrivate) {
-      return "return";
-    }
-
-    // if (this.toolsSetData.permission == "master" && !this.e.isMaster) {
-    //   return "return";
-    // }
-
-    // if (this.toolsSetData.permission == "admin" && !this.e.member.is_admin) {
-    //   return "return";
-    // }
-    // if (this.toolsSetData.permission == "owner" && !this.e.member.is_owner) {
-    //   return "return";
-    // }
-
-    if (this.toolsSetData.cd != 0) {
-      /** cd */
-      let key = `Yz:woc:${this.e.group_id}`;
-      if (await redis.get(key)) return;
-      redis.set(key, "1", { EX: Number(this.toolsSetData.cd) });
-    }
-
-    this.e.reply("触发探索未知的神秘空间，请稍等...");
-    let images = [];
-    const isDimtown = this.toolsSetData.wocUrl.indexOf("dimtown.com") !== -1;
-    if (this.toolsSetData.wocUrl.indexOf("/wp-json") !== -1) {
-      const randomMax = isDimtown ? 400 : 600;
-
-      const randomIndex = Math.floor(Math.random() * randomMax) + 1;
-
-      const page = Math.ceil(randomIndex / 10);
-
-      const fetchData = await fetch(`${this.toolsSetData.wocUrl}${page}`);
-      const resJsonData = await fetchData.json();
-
-      const index = randomIndex % 10;
-
-      if (!resJsonData.length) {
-        this.e.reply("额。没有探索到，换个姿势再来一次吧～");
-        return "return";
-      }
-
-      const content = resJsonData[index].content;
-
-      if (!content || !content.rendered) {
-        this.e.reply("额。没有探索到，换个姿势再来一次吧～");
-        return "return";
-      }
-
-      images = this.getImages(content.rendered);
-    } else {
-      if (urlTypeCache[this.toolsSetData.wocUrl] == "buffer") {
-        const image = await this.getBufferImage(`${this.toolsSetData.wocUrl}`);
-        images = [image];
-      } else {
-        try {
-          const fetchData = await fetch(`${this.toolsSetData.wocUrl}`);
-          const resJsonData = await fetchData.json();
-
-          images = this.getJsonImages(JSON.stringify(resJsonData));
-        } catch (error) {
-          urlTypeCache[this.toolsSetData.wocUrl] = "buffer";
-          const image = await this.getBufferImage(
-            `${this.toolsSetData.wocUrl}`
-          );
-          images = [image];
-        }
-      }
-    }
-
-    if (isDimtown && images.length > 1) {
-      images.pop();
-    }
-
-    const imageCountLimit = this.toolsSetData.imageCountLimit || 10;
-
-    if (images.length > imageCountLimit) {
-      images.length = imageCountLimit;
-    }
-
-    const forwarder =
-      this.toolsSetData.forwarder == "bot"
-        ? { nickname: Bot.nickname, user_id: Bot.uin }
-        : {
-            nickname: this.e.sender.card || this.e.user_id,
-            user_id: this.e.user_id,
-          };
-
-    if (images && images.length) {
-      let msgList = [];
-      for (let imageItem of images) {
-        if (isPrivate) {
-          await this.e.reply(segment.image(imageItem), false, {
-            recallMsg: this.toolsSetData.delMsg,
-          });
-          await common.sleep(600);
-        } else {
-          msgList.push({
-            message: segment.image(imageItem),
-            ...forwarder,
-          });
-        }
-      }
-      if (isPrivate) {
-        return;
-      }
-      const res = await this.e.reply(await Bot.makeForwardMsg(msgList), false, {
-        recallMsg: this.toolsSetData.delMsg,
-      });
-      if (!res) {
-        if (!res) {
-          if (this.e.group.is_admin) {
-            if (
-              Number(Math.random().toFixed(2)) * 100 <
-              this.toolsSetData.mute
-            ) {
-              let duration = Math.floor(Math.random() * 600) + 1;
-              this.e.group.muteMember(this.e.sender.user_id, duration);
-              await this.e.reply(
-                `不用等了，你想要的已经被神秘的力量吞噬了～ 并随手将你禁锢${duration}秒`
-              );
-            } else {
-              this.reply("不用等了，你想要的已经被神秘的力量吞噬了～");
-            }
-          } else {
-            this.reply("不用等了，你想要的已经被神秘的力量吞噬了～");
-          }
-        }
-      }
-    } else {
-      this.reply("额。没有探索到，换个姿势再来一次吧～");
-    }
   }
 
   async fdrank() {
@@ -235,45 +70,5 @@ export class tools extends plugin {
       limitTop: 10,
     });
     this.e.reply(img);
-  }
-
-  getImages(string) {
-    const imgRex = /<img.*?src="(.*?)"[^>]+>/g;
-    const images = [];
-    let img;
-    while ((img = imgRex.exec(string))) {
-      images.push(img[1]);
-    }
-    return images;
-  }
-
-  getJsonImages(string) {
-    const imgRex = /https?:\/\/.*?\.(jpg|JPG|png|PNG|gif|GIF|jpeg|JPEG)/g;
-    const images = [];
-    let img;
-    while ((img = imgRex.exec(string))) {
-      images.push(img[0]);
-    }
-    return images;
-  }
-
-  // 获取图片
-  async getBufferImage(url) {
-    let response = await fetch(url, {
-      method: "get",
-      responseType: "arraybuffer",
-    });
-
-    const buffer = await response.arrayBuffer();
-
-    return (
-      "base64://" +
-      btoa(
-        new Uint8Array(buffer).reduce(
-          (data, byte) => data + String.fromCharCode(byte),
-          ""
-        )
-      )
-    );
   }
 }
