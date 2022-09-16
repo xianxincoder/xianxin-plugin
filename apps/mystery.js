a25f0f34d8b076ead6eda1f360379b8d47786d74
闲心 <11329420+xianxincoder@user.noreply.gitee.com>
2022-09-16 13:25:10 +0000

    feat: 跟换默认源
    Signed-off-by: 闲心 <11329420+xianxincoder@user.noreply.gitee.com>

--- apps/mystery.js
+++ apps/mystery.js
@@ -313,7 +313,7 @@ export class mystery extends plugin {
     let url = this.e.msg.replace(/#*(神秘)?(pro)?换源\s*/g, "") || "";
     if (url == "") {
       url = isPro
-        ? "http://api.xn--7gqa009h.top/api/nysp?key=qiqi"
+        ? "https://xiaobai.klizi.cn/API/video/spzm.php?data=&msg=美女"
         : "https://yingtall.com/wp-json/wp/v2/posts?page=";
     }
 
