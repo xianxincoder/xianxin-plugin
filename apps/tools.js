4b592a0e178cc7ab5ab7133906463dc64258b529
闲心 <11329420+xianxincoder@user.noreply.gitee.com>
2022-09-27 03:56:03 +0000

    feat: 添加 #官威列表、#官威删除+内容 管理已添加的(戳|撤|禁|踢)项目
    Signed-off-by: 闲心 <11329420+xianxincoder@user.noreply.gitee.com>

--- apps/tools.js
+++ apps/tools.js
@@ -1,5 +1,6 @@
 import plugin from "../../../lib/plugins/plugin.js";
 import fetch from "node-fetch";
+import { segment } from "oicq";
 import puppeteer from "../../../lib/puppeteer/puppeteer.js";
 import Tools from "../model/tools.js";
 import lodash from "lodash";
@@ -24,6 +25,14 @@ export class tools extends plugin {
         {
           reg: "^(戳|撤|禁|踢)$",
           fnc: "shortcuts",
+        },
+        {
+          reg: "^#*官威删除(.*)$",
+          fnc: "shortcutsdel",
+        },
+        {
+          reg: "^#*官威列表$",
+          fnc: "shortcutslist",
         },
         {
           reg: "^#*(闲心)?发电榜$",
@@ -88,24 +97,7 @@ export class tools extends plugin {
   }
 
   async shortcuts() {
-    if (
-      this.toolsSetData.shortcutsPermission == "owner" &&
-      !(this.e.member.is_owner || this.e.isMaster)
-    ) {
-      return;
-    }
-
-    if (
-      this.toolsSetData.shortcutsPermission == "admin" &&
-      !(this.e.member.is_owner || this.e.isMaster || this.e.member.is_admin)
-    ) {
-      return;
-    }
-
-    if (this.toolsSetData.shortcutsPermission == "master" && !this.e.isMaster) {
-      return;
-    }
-
+    if (!this.checkAuth()) return;
     await this.getGroupId();
 
     if (!this.group_id) {
@@ -142,6 +134,126 @@ export class tools extends plugin {
         this.e.group.pokeMember(this.e.source.user_id);
       }
     }
+  }
+
+  async shortcutsdel() {
+    if (!this.checkAuth()) return;
+    await this.getGroupId();
+    if (!this.group_id) return false;
+
+    this.initTextArr();
+
+    let keyWord = this.getKeyWord(this.e).replace(/#|\n|＃|官威删除/g, "");
+
+    let temp = textArr[this.group_id].get(keyWord);
+    if (textArr[this.group_id].has(keyWord)) {
+      textArr[this.group_id].delete(keyWord);
+    }
+
+    let retMsg = [{ type: "text", text: "删除成功：" }];
+    for (let msg of this.e.message) {
+      if (msg.type == "text") {
+        msg.text = msg.text.replace(/#|＃|官威删除/g, "");
+
+        if (!msg.text) continue;
+      }
+      retMsg.push(msg);
+    }
+
+    await this.e.reply(retMsg);
+
+    /** 删除图片 */
+    let img = temp;
+    if (Array.isArray(temp)) {
+      img = item[0];
+    }
+    if (img && img.local) {
+      fs.unlink(img.local, () => {});
+    }
+
+    this.saveJson();
+  }
+
+  async shortcutslist() {
+    let page = 1;
+    let pageSize = 100;
+    let type = "list";
+
+    await this.getGroupId();
+    if (!this.group_id) return false;
+
+    this.initTextArr();
+
+    let search = this.e.msg.replace(/#|＃|官威/g, "");
+
+    if (search.includes("列表")) {
+      page = search.replace(/列表/g, "") || 1;
+    } else {
+      type = "search";
+    }
+
+    let list = textArr[this.group_id];
+
+    if (lodash.isEmpty(list)) {
+      await this.e.reply("暂无官威数据");
+      return;
+    }
+
+    let arr = [];
+    for (let [k, v] of textArr[this.group_id]) {
+      if (type == "list") {
+        arr.push({ key: k, val: v, num: arr.length + 1 });
+      } else if (k.includes(search)) {
+        /** 搜索官威 */
+        arr.push({ key: k, val: v, num: arr.length + 1 });
+      }
+    }
+
+    let count = arr.length;
+    arr = arr.reverse();
+
+    if (type == "list") {
+      arr = this.pagination(page, pageSize, arr);
+    }
+
+    if (lodash.isEmpty(arr)) {
+      return;
+    }
+
+    let msg = [];
+    let num = 0;
+    for (let i in arr) {
+      if (num >= page * pageSize) break;
+
+      let keyWord = await this.keyWordTran(arr[i].key);
+      if (!keyWord) continue;
+
+      // if (Array.isArray(keyWord)) {
+      //   keyWord.unshift(`${arr[i].num}、`);
+      //   keyWord.push("\n");
+      //   keyWord.forEach((v) => msg.push(v));
+      // } else
+      if (keyWord.type) {
+        msg.push(`\n${arr[i].num}、`, keyWord, `[${arr[i].val}]`, "\n\n");
+      } else {
+        msg.push(`${arr[i].num}、${keyWord}[${arr[i].val}]\n`);
+      }
+      num++;
+    }
+
+    let end = "";
+    if (type == "list" && count > 100) {
+      end = `更多内容请翻页查看\n如：#官威列表${Number(page) + 1}`;
+    }
+
+    let title = `官威列表，第${page}页，共${count}条`;
+    if (type == "search") {
+      title = `官威${search}，${count}条`;
+    }
+
+    let forwardMsg = await this.makeForwardMsg(Bot.uin, title, msg, end);
+
+    this.e.reply(forwardMsg);
   }
 
   async fdrank() {
@@ -182,6 +294,115 @@ export class tools extends plugin {
       limitTop: 10,
     });
     this.e.reply(img);
+  }
+
+  checkAuth() {
+    if (this.e.isMaster) return true;
+
+    if (
+      this.toolsSetData.shortcutsPermission == "owner" &&
+      !(this.e.member.is_owner || this.e.isMaster)
+    ) {
+      return true;
+    }
+
+    if (
+      this.toolsSetData.shortcutsPermission == "admin" &&
+      !(this.e.member.is_owner || this.e.isMaster || this.e.member.is_admin)
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+  async makeForwardMsg(qq, title, msg, end = "") {
+    let nickname = Bot.nickname;
+    if (this.e.isGroup) {
+      let info = await Bot.getGroupMemberInfo(this.e.group_id, qq);
+      nickname = info.card ?? info.nickname;
+    }
+    let userInfo = {
+      user_id: Bot.uin,
+      nickname,
+    };
+
+    let forwardMsg = [
+      {
+        ...userInfo,
+        message: title,
+      },
+    ];
+
+    let msgArr = lodash.chunk(msg, 40);
+    msgArr.forEach((v) => {
+      v[v.length - 1] = lodash.trim(v[v.length - 1], "\n");
+      forwardMsg.push({ ...userInfo, message: v });
+    });
+
+    if (end) {
+      forwardMsg.push({ ...userInfo, message: end });
+    }
+
+    /** 制作转发内容 */
+    if (this.e.isGroup) {
+      forwardMsg = await this.e.group.makeForwardMsg(forwardMsg);
+    } else {
+      forwardMsg = await this.e.friend.makeForwardMsg(forwardMsg);
+    }
+
+    /** 处理描述 */
+    forwardMsg.data = forwardMsg.data
+      .replace(/\n/g, "")
+      .replace(/<title color="#777777" size="26">(.+?)<\/title>/g, "___")
+      .replace(/___+/, `<title color="#777777" size="26">${title}</title>`);
+
+    return forwardMsg;
+  }
+
+  /** 分页 */
+  pagination(pageNo, pageSize, array) {
+    let offset = (pageNo - 1) * pageSize;
+    return offset + pageSize >= array.length
+      ? array.slice(offset, array.length)
+      : array.slice(offset, offset + pageSize);
+  }
+
+  /** 关键词转换成可发送消息 */
+  async keyWordTran(msg) {
+    /** 图片 */
+    if (msg.includes("{image")) {
+      let tmp = msg.split("{image");
+      if (tmp.length > 2) return false;
+
+      let md5 = tmp[1].replace(/}|_|:/g, "");
+
+      msg = segment.image(`http://gchat.qpic.cn/gchatpic_new/0/0-0-${md5}/0`);
+      msg.asface = true;
+    } else if (msg.includes("{at:")) {
+      let tmp = msg.match(/{at:(.+?)}/g);
+
+      for (let qq of tmp) {
+        qq = qq.match(/[1-9][0-9]{4,14}/g)[0];
+        let member = await await Bot.getGroupMemberInfo(
+          this.group_id,
+          Number(qq)
+        ).catch(() => {});
+        let name = member?.card ?? member?.nickname;
+        if (!name) continue;
+        msg = msg.replace(`{at:${qq}}`, `@${name}`);
+      }
+    } else if (msg.includes("{face")) {
+      let tmp = msg.match(/{face(:|_)(.+?)}/g);
+      if (!tmp) return msg;
+      msg = [];
+      for (let face of tmp) {
+        let id = face.match(/\d+/g);
+        msg.push(segment.face(id));
+      }
+    }
+
+    return msg;
   }
 
   saveJson() {
