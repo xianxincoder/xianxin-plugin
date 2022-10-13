eeb828ac6b31de16c893607815b610c545e46a2c
闲心 <xianxincoder@outlook.com>
2022-10-13 08:31:50 +0000

    feat: 添加 #潜水 小工具插件潜水群友
    Signed-off-by: 闲心 <xianxincoder@outlook.com>

--- apps/tools.js
+++ apps/tools.js
@@ -1,4 +1,7 @@
 import plugin from "../../../lib/plugins/plugin.js";
+import common from "../../../lib/common/common.js";
+import moment from "moment";
+import lodash from "lodash";
 
 export class tools extends plugin {
   constructor() {
@@ -12,6 +15,11 @@ export class tools extends plugin {
           reg: "^#*赞我$",
           fnc: "thumbsUpMe",
         },
+        {
+          reg: "^#*潜水\\s*[0-9]*$",
+          fnc: "lurk",
+          permission: "master",
+        },
       ],
     });
   }
@@ -23,5 +31,51 @@ export class tools extends plugin {
   async thumbsUpMe() {
     Bot.pickFriend(this.e.user_id).thumbUp(10);
     this.e.reply("已给你点赞");
+  }
+
+  async lurk() {
+    let days = this.e.msg.replace(/#*潜水\s*/g, "").trim() || 0;
+
+    let gl = await this.e.group.getMemberMap();
+
+    let msg = [];
+
+    for (let [k, v] of gl) {
+      if (days == 0) {
+        if (v.last_sent_time == v.join_time) {
+          //计算相差多少天
+          let diffDay = moment().diff(moment.unix(v.join_time), "day");
+          msg.push(
+            `${v.nickname}(${v.user_id}) 入群${diffDay}天，一直在潜水。`
+          );
+        }
+      } else {
+        //计算相差多少天
+        let diffDay = moment().diff(moment.unix(v.last_sent_time), "day");
+        if (diffDay >= days) {
+          msg.push(
+            `${v.nickname}(${v.user_id}) 已潜水${diffDay}天了，该出来冒个泡啦！`
+          );
+        }
+      }
+    }
+
+    let total = msg.length;
+
+    const msgArr = lodash.chunk(msg, 10);
+
+    const groupmsg = msgArr.map((item) => {
+      return item.join("\n");
+    });
+
+    msg = await common.makeForwardMsg(
+      this.e,
+      groupmsg,
+      days
+        ? `潜水超过${days}天的群友共${total}个`
+        : `入群从未发言的群友共${total}个`
+    );
+
+    await this.e.reply(msg);
   }
 }
