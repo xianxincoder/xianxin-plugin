f58d823e0bf1d42609a5e81d8ffcfe70a2d39c1a
闲心 <xianxincoder@outlook.com>
2022-11-25 00:54:27 +0000

    feat: 优化插件载入速度
    Signed-off-by: 闲心 <xianxincoder@outlook.com>

--- index.js
+++ index.js
@@ -11,10 +11,23 @@ const files = fs
   .readdirSync("./plugins/xianxin-plugin/apps")
   .filter((file) => file.endsWith(".js"));
 
-let apps = {};
-for (let file of files) {
-  let name = file.replace(".js", "");
-  apps[name] = (await import(`./apps/${file}`))[name];
-}
+let ret = [];
 
+files.forEach((file) => {
+  ret.push(import(`./apps/${file}`));
+});
+
+ret = await Promise.allSettled(ret);
+
+let apps = {};
+for (let i in files) {
+  let name = files[i].replace(".js", "");
+
+  if (ret[i].status != "fulfilled") {
+    logger.error(`载入插件错误：${logger.red(name)}`);
+    logger.error(ret[i].reason);
+    continue;
+  }
+  apps[name] = ret[i].value[Object.keys(ret[i].value)[0]];
+}
 export { apps };
